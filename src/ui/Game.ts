95ed2221740f551b5f1dd9e62e4d81846beaf5ee
phil@home <philipp.millner@outlook.com>
2024-08-22 22:55:00 +0200

    fix: removed p tag which enclosed every element

--- src/ui/Game.ts
+++ src/ui/Game.ts
@@ -6,27 +6,27 @@ import "./components/resources/GameResources";
 
 @customElement("game-element")
 export class MainComponent extends GameComponent {
-
   connectedCallback() {
     super.connectedCallback();
   }
 
   render() {
     return html`
-      <p>
-        <game-header 
-          .player=${this.gameState.player}
-          .gold=${this.gameState.resources.GOLD.stored}
-          .population=${this.gameState.resources.POPULATION.stored}
-          .maxPopulation=${this.gameState.resources.POPULATION.maxStorage!}
-          .timeToIncPopulation=${this.gameState.populationGenTime}
-          .onTimerEnd=${() => {
-            const { resourceActions, resources: { POPULATION } } = this.gameState;
-            resourceActions.produce("POPULATION", POPULATION.productionValues.perSecond);
-          }}
-        ></game-header>
-        <game-resources></game-resources>
-      </p>
+      <game-header
+        .player=${this.gameState.player}
+        .gold=${this.gameState.resources.GOLD.stored}
+        .population=${this.gameState.resources.POPULATION.stored}
+        .maxPopulation=${this.gameState.resources.POPULATION.maxStorage!}
+        .timeToIncPopulation=${this.gameState.populationGenTime}
+        .onTimerEnd=${() => {
+          const {
+            resourceActions,
+            resources: { POPULATION },
+          } = this.gameState;
+          resourceActions.produce("POPULATION", POPULATION.productionValues.perSecond);
+        }}
+      ></game-header>
+      <game-resources></game-resources>
     `;
   }
 }
